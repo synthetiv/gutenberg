8c3b07ae5f3ee6bc7e636c23623f84d2558a3812
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2021-07-08 14:13:27 +0200

    Testing: Upgrade Jest to v27

--- packages/block-editor/src/utils/test/parse-css-unit-to-px.js
+++ packages/block-editor/src/utils/test/parse-css-unit-to-px.js
@@ -6,6 +6,8 @@ import {
 	getPxFromCssUnit,
 } from '../parse-css-unit-to-px';
 
+jest.useRealTimers();
+
 describe( 'getPxFromCssUnit', () => {
 	// Absolute units
 	it( 'test px return px unit', () => {
