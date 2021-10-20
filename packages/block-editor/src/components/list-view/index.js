cb18edc35bd6e15806862877e66eadf3443aaa42
ramonjd <ramonjd@gmail.com>
2021-10-20 15:42:53 +1100

    Check element ref for active document so we can try to expand the list only when a block is selected outside the block list. Using an `useEffect` to loop through parent nodes, and expand them when necessary.

--- packages/block-editor/src/components/list-view/index.js
+++ packages/block-editor/src/components/list-view/index.js
@@ -24,6 +24,7 @@ import ListViewDropIndicator from './drop-indicator';
 import useListViewClientIds from './use-list-view-client-ids';
 import useListViewDropZone from './use-list-view-drop-zone';
 import { store as blockEditorStore } from '../../store';
+import { hasFocusWithin } from './utils';
 
 const noop = () => {};
 const expanded = ( state, action ) => {
@@ -65,7 +66,11 @@ function ListView(
 	},
 	ref
 ) {
-	const { clientIdsTree, draggedClientIds } = useListViewClientIds( blocks );
+	const {
+		clientIdsTree,
+		draggedClientIds,
+		selectedBlockParentIds,
+	} = useListViewClientIds( blocks );
 	const { selectBlock } = useDispatch( blockEditorStore );
 	const selectEditorBlock = useCallback(
 		( clientId ) => {
@@ -75,12 +80,12 @@ function ListView(
 		[ selectBlock, onSelect ]
 	);
 	const [ expandedState, setExpandedState ] = useReducer( expanded, {} );
-
 	const { ref: dropZoneRef, target: blockDropTarget } = useListViewDropZone();
 	const elementRef = useRef();
 	const treeGridRef = useMergeRefs( [ elementRef, dropZoneRef, ref ] );
-
 	const isMounted = useRef( false );
+	const hasFocus = hasFocusWithin( elementRef?.current );
+
 	useEffect( () => {
 		isMounted.current = true;
 	}, [] );
@@ -138,6 +143,26 @@ function ListView(
 			collapse,
 		]
 	);
+
+	// If a selection is made outside the block list,
+	// for example, in the Block Editor,
+	// try to expand the block list tree.
+	useEffect( () => {
+		if (
+			! hasFocus &&
+			Array.isArray( selectedBlockParentIds ) &&
+			selectedBlockParentIds.length
+		) {
+			selectedBlockParentIds.forEach( ( clientId ) => {
+				if ( ! expandedState[ clientId ] ) {
+					setExpandedState( {
+						type: 'expand',
+						clientId,
+					} );
+				}
+			} );
+		}
+	}, [ hasFocus, selectedBlockParentIds ] );
 
 	return (
 		<AsyncModeProvider value={ true }>
