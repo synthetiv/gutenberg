585fc17e30b24ae04e16e343966bc1f495572537
ramonjd <ramonjd@gmail.com>
2021-10-27 15:06:12 +1100

    Check for existing data-id attribute on the image block content

--- packages/block-library/src/image/index.php
+++ packages/block-library/src/image/index.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * Server-side rendering of the `core/image` block.
+ *
+ * @package gutenberg
+ */
+
+/**
  * Renders the `core/image` block on server.
  *
  * @param  array $attributes The block attributes.
@@ -10,7 +16,9 @@ function render_block_core_image( $attributes, $content ) {
 	if ( $attributes['id'] ) {
 		// Add the data-id="$id" attribute to the img element to retain backwards compatibility.
 		$data_id_attribute  = 'data-id="' . esc_attr( $attributes['id'] ) . '"';
-		$content = str_replace( '<img', '<img ' . $data_id_attribute . ' ', $content );
+		if ( ! strpos( $content, $data_id_attribute ) ) {
+			$content = str_replace( '<img', '<img ' . $data_id_attribute . ' ', $content );
+		}
 	}
 	return $content;
 }
